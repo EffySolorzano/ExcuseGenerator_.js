b92415ebab407e4a3de76069559c39d53e4830d0
EffySolorzano <solorzano.steph@gmail.com>
2023-02-07 22:42:37 +0000

    firstcommit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+
+  let subject = [
+    "An alligator",
+    "Florida man",
+    "Aliens",
+    "A herd of wild goats",
+    "A human-sized raccoon",
+    
+  
+  return "A dog chew my laptop at a McDonalds";
 };
